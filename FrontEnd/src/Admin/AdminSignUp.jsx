f2eedfd7822e4d0a298a7bb4e369ddc799500ca8
Devaprakash47 <deva27997@gmail.com>
2025-07-02 22:58:45 +0530

    Initial commit

--- FrontEnd/src/Admin/AdminSignUp.jsx
+++ FrontEnd/src/Admin/AdminSignUp.jsx
@@ -11,8 +11,20 @@ function AdminSignUp({ onClose }) {
   const handleSignup = (e) => {
     e.preventDefault();
 
-    axios.post('http://localhost:3001/admin', { username , email, password })
-      
+    axios.post('http://localhost:3001/admin', { username,email, password })
+      .then(res => {
+        if (res.data.success) {
+          alert('register successfully!');
+          navigate('/admin-signin');
+          onClose(); 
+        } else {
+          alert('Invalid credentials');
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        alert('Error during login');
+      });
       setUsername('');
       setemail('');
       setPassword('');
@@ -28,28 +40,28 @@ function AdminSignUp({ onClose }) {
             placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            
+            required
           />
           <input
-            type='email'
-            placeholder="EmailID"
+            type="email"
+            placeholder="Email"
             value={email}
             onChange={(e) => setemail(e.target.value)}
-            
+            required
           />
           <input
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-          
+            required
           />
           <div>
             <p>Already have account?</p>
             <Link to="/admin-signin">Sign In</Link>
           </div>
           <div>
-            <button onClick={handleSignup}>Access</button>
+            <button onClick={handleSignup}>Register</button>
             <button type="button" onClick={onClose}>Close</button>
           </div>
         
