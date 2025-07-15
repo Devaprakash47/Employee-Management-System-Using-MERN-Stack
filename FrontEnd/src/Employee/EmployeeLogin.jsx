cb4eeb50cb8f9a367444ade3a45e28cf280c50a6
Devaprakash47 <deva27997@gmail.com>
2025-07-15 23:35:24 +0530

    Initial Commit

--- FrontEnd/src/Employee/EmployeeLogin.jsx
+++ FrontEnd/src/Employee/EmployeeLogin.jsx
@@ -3,18 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function EmployeeLogin({ onClose }) {
-  const [empId, setEmpId] = useState('');
+  const [email, setEmail] = useState('');
   const [dob, setDob] = useState('');
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    if (!empId.trim() || !dob.trim()) {
-      return alert('Please enter Employee ID and DOB');
+    if (!email.trim() || !dob.trim()) {
+      return alert('Please enter Email and DOB');
     }
 
     try {
       const res = await axios.post('http://localhost:3001/api/employee-login', {
-        empId,
+        email,
         dob
       }, { withCredentials: true });
 
@@ -34,10 +34,10 @@ function EmployeeLogin({ onClose }) {
       <div className="modal-content">
         <h2>Employee Login</h2>
         <input
-          type="text"
-          placeholder="Employee ID"
-          value={empId}
-          onChange={(e) => setEmpId(e.target.value)}
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="date"
